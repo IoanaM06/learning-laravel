7825ddb5d8b30bd75bb3b0aad932d8d8540d4c87
ioana <ioana.antonia.m@gmail.ca>
2024-03-06 15:05:12 -0500

    completion of lesson 16
    completion of lesson 15
    
    completion of lesson 15

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,19 +1,9 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My blog</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
+<x-layout>
     <article>
-        <h1><?= $post->title; ?></h1>
+        <h1>{{$post->title}}</h1>
 
-        <div><?= $post->body?></div>
+        <div>{!!$post->body!!}</div>
     </article>
 
     <a href="/">Go Back</a>
-</body>
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,22 +1,14 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>My blog</title>
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-</head>
-<body>
-    <?php foreach ($posts as $post): ?>
+<x-layout>
+    @foreach ($posts as $post)
         <article>
             <h1>
-                <a href="/posts/<?= $post->slug; ?>"><?= $post->title; ?></a>
+                <a href="/posts/{{$post->slug}}">
+                    {{$post->title}}
+                </a>
             </h1>
             <div>
-                <p><?= $post->excerpt; ?></p>
+                <p>{{$post->excerpt}}</p>
             </div>
         </article>
-    <?php endforeach; ?>
-</body>
-</html>
+    @endforeach
+</x-layout>
