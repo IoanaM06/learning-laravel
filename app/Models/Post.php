ea504592bb260024000a9add04de67f89dfa629c
ioana <ioana.antonia.m@gmail.ca>
2024-03-06 14:59:55 -0500

    just in case

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -50,4 +50,15 @@ class Post
         return static::all()->firstWhere("slug", $slug);
     }
 
+    // finds a specific post according to it's slug and throws 404 if not found
+    public static function findOrFail($slug)
+    {
+        $post = static::find($slug);
+        if (! $post) {
+            throw new ModelNotFoundException();
+        }
+
+        return $post;
+    }
+
 }
