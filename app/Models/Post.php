a569dc6a61be7a7b99d242559e5c810f2c54c14c
ioana <ioana.antonia.m@gmail.ca>
2024-03-21 13:48:50 -0400

    completed 36

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,7 +11,7 @@ class Post extends Model
 
     protected $guarded = [];
 
-    protected $with = ['category', 'author'];
+    protected $fillable = ["title", "slug", "excerpt", "body", "category_id", "user_id", "username"];
 
     public function category()
     {
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -24,8 +24,8 @@ class PostFactory extends Factory
             'category_id' => Category::factory(),
             'title' => $this->faker->sentence,
             'slug' => $this->faker->slug,
-            'excerpt' => $this->faker->sentence,
-            'body' => $this->faker->paragraph,
+            'excerpt' => '<p>' . $this->faker->paragraph(5) . '</p>',
+            'body' => '<p>' . $this->faker->paragraph(16) . '</p>',
         ];
     }
 }
