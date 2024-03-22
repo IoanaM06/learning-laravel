a9f7c518a4c933192d1028d8406e2a108b39d5b4
ioana <ioana.antonia.m@gmail.ca>
2024-03-22 14:46:20 -0400

    44 done

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,19 +1,14 @@
 <?php
-
 namespace App\Http\Controllers;
-
-use App\Models\Category;
 use App\Models\Post;
-use Illuminate\Http\Request;
-
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::latest();
-
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get(),
+            'posts' => Post::latest()->filter(
+                        request(['search', 'category', 'author'])
+                    )->simplePaginate(9)->withQueryString()
         ]);
     }
 
